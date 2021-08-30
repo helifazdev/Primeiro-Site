430266267bb154d7d1b51a50480da48ac1602ae2
Helifaz Rocha <76110177+helifazdev@users.noreply.github.com>
2021-08-29 21:41:28 -0300

    AsupeG29082021

--- js/main.js
+++ js/main.js
@@ -1,2 +1,2 @@
-var versao = ("Asupejs29082021")
+var versao = ("AsupeG29082021")
 alert("Olá obrigado por nos visitar!\nO site ainda está em desenvolvimento \nVersão Atual: " + versao + "\nDev:helifaz Rocha");
