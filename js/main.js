ad3c12fd1d55a211aa329eab423206615be489b7
helifazdev <helifazsilva@gmail.com>
2021-09-06 00:56:27 -0300

    Novo Asupe responsivo,

--- js/main.js
+++
@@ -1,3 +0,0 @@
-var versao = ("AsupeG29082021")
-var data = new Date;
-alert("Olá obrigado por nos visitar!\nO site ainda está em desenvolvimento \nVersão Atual: " + versao + "\nDev:helifaz Rocha\n" + data);
